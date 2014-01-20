10bc10b2a09bd587c42f27a656ec7b16265b9914
Prescott Murphy <prurph@gmail.com>
2014-01-20 18:58:14 -0500

    Write tests for todo items

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,4 +1,6 @@
 class TodoItemsController < ApplicationController
   def index
+    # Get access to this in todo_items index.html.erb file
+    @todo_list = TodoList.find(params[:todo_list_id])
   end
 end
